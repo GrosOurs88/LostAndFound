0c5d38498073aacca5a6d7afd6c683de05050913
Hugo Bourbon <hugo.bourbon@fishingcactus.com>
2021-03-29 18:04:07 +0200

    Change materials + lighting

--- Assets/Scripts/MasterScript.cs
+++ Assets/Scripts/MasterScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MasterScript : MonoBehaviour
 {
     public Light globalLight;
+    public List<Light> lights = new List<Light>();
 
     public List<GameObject> sectionsList = new List<GameObject>();
 
@@ -16,14 +17,23 @@ public class MasterScript : MonoBehaviour
     public IEnumerator GLobalEnvironmentInstantiation()
     {
         globalLight.GetComponent<Light>().enabled = true;
+        foreach(Light l in lights)
+        {
+            l.GetComponent<Light>().enabled = false;
+        }
 
         for (int i = 0; i < sectionsList.Count; i++)
         {
             sectionsList[i].GetComponent<InstantiateObjectsScript>().SetupEnvironment();
         }
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.25f);
+
         globalLight.GetComponent<Light>().enabled = false;
+        foreach (Light l in lights)
+        {
+            l.GetComponent<Light>().enabled = true;
+        }
 
         yield return null;
     }
