d8406e7f5951b6d63f671a4d6db9ad9d56a9b180
Hugo Bourbon <hugo.bourbon@fishingcactus.com>
2021-04-15 23:08:38 +0200

    Add new shader and meshes

--- Assets/Scripts/TrapTriggerScript.cs
+++ Assets/Scripts/TrapTriggerScript.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class TrapTriggerScript : MonoBehaviour
 {
     public GameObject trap;
-
+    public List<ParticleSystem> poisonThrowers;
+    public ParticleSystem PoisonousGaz;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             trap.GetComponent<MoveDeathObjectScript>().isFalling = true;
+
+            foreach(ParticleSystem pS in poisonThrowers)
+            {
+                pS.Play();
+            }
+
+            PoisonousGaz.Play();
 
             gameObject.SetActive(false);
         }
