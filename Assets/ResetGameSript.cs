cb2ee2732e9afec515c749a6c93b3cfca19983a7
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2021-10-31 22:25:08 +0100

    Add a reset script to avoid the F9

---
+++ Assets/ResetGameSript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGameSript : MonoBehaviour
+{
+    public KeyCode inputReset;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(inputReset))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
