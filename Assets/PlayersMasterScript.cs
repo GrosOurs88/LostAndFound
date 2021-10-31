404ec7d278ac53de659944794e47bb075442284a
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2021-10-31 22:25:46 +0100

    Add a debug line for the detected controllers

--- Assets/PlayersMasterScript.cs
+++ Assets/PlayersMasterScript.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayersMasterScript : MonoBehaviour
 {
     public GameObject[] playersPrefabList;
     public Transform[] playersStartingPositionsList;
 
+    public TextMeshProUGUI controllersDebug;
+
     void Start()
     {
         string[] names = Input.GetJoystickNames();
 
         print("There are : " + names.Length + " controllers connected");
+        controllersDebug.text = names.ToString();
 
         for (int x = 0; x < names.Length; x++)
         {
