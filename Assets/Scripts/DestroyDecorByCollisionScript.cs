3eb0ce99550e0793e5e043c9274cb9ad3be8f6c7
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2024-11-07 23:35:51 +0100

    Clean every asset folder and rename assets

---
+++ Assets/Scripts/DestroyDecorByCollisionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDecorByCollisionScript : MonoBehaviour
+{
+    public float timer = 2f;
+
+    private void Update()
+    {
+        if(Time.time == timer)
+        {
+            MakesObjectKinematicThenRemoveScript();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Decor"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void MakesObjectKinematicThenRemoveScript()
+    {
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+        Destroy(gameObject.GetComponent<DestroyDecorByCollisionScript>());
+    }
+}
---
+++ Assets/Scripts/DestroyMapsByCollisionScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMapsByCollisionScript : MonoBehaviour
+{
+    public ParticleSystem mapBurnedParticle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Map"))
+        {
+            Instantiate(mapBurnedParticle, collision.GetContact(0).point, Quaternion.identity);
+            Destroy(collision.gameObject);
+        }
+    }
+}
